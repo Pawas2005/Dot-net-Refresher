f54949efcdc9fa006d47fcff9f554c2b40e84c8e
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-02 00:07:26 +0530

    String Format

---
+++ DAY2/TopBrains/StringFormat/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using System.Collections.Generic;
+
+public record Student(string Name, int Score);
+
+public class Solution
+{
+    public static string GetStudentsJson(string[] items, int minScore)
+    {
+        List<Student> students = new List<Student>();
+
+        foreach (string item in items)
+        {
+            string[] parts = item.Split(':');
+
+            students.Add(new Student(parts[0], int.Parse(parts[1])));
+        }
+
+        var result = students
+            .Where(s => s.Score >= minScore)
+            .OrderByDescending(s => s.Score)
+            .ThenBy(s => s.Name)
+            .ToList();
+
+        return JsonSerializer.Serialize(result);
+    }
+
+    public static void Main()
+    {
+        string[] items =
+        {
+            "John:80",
+            "Alice:95",
+            "Bob:75",
+            "David:95",
+            "Chris:60"
+        };
+
+        int minScore = 75;
+
+        string json = GetStudentsJson(items, minScore);
+
+        Console.WriteLine(json);
+    }
+}
