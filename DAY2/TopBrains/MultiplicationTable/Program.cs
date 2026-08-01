8457289db82787c623a952ec37532d6d5be5fd36
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-01 23:53:39 +0530

    Multiplication Table

---
+++ DAY2/TopBrains/MultiplicationTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Solution
+{
+    public static int[] MultiplicationRow(int n, int upto)
+    {
+        int[] row = new int[upto];
+
+        for (int i = 1; i <= upto; i++)
+        {
+            row[i - 1] = n * i;
+        }
+
+        return row;
+    }
+
+    public static void Main()
+    {
+        int n = 3;
+        int upto = 5;
+
+        int[] result = MultiplicationRow(n, upto);
+
+        Console.WriteLine("[" + string.Join(",", result) + "]");
+    }
+}
