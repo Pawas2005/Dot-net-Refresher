6437ea5e5c101b8beb75b5c25ea9f4a003189965
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-02 12:57:27 +0530

    Largest Integer

---
+++ DAY2/TopBrains/Largest3Int/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Solution
+{
+    public static int LargestInteger(int a, int b, int c)
+    {
+        if (a >= b && a >= c)
+            return a;
+        else if (b >= a && b >= c)
+            return b;
+        else
+            return c;
+    }
+
+    public static void Main()
+    {
+        int a = 10;
+        int b = 25;
+        int c = 15;
+
+        Console.WriteLine(LargestInteger(a, b, c));
+    }
+}
