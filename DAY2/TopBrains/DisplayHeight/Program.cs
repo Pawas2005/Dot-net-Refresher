f732aeed8549c45ed50c87bb0144617b0aaa83f3
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-02 23:09:39 +0530

    Display Height

---
+++ DAY2/TopBrains/DisplayHeight/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Solution
+{
+    public string DisplayHeight(int heightCm)
+    {
+        if (heightCm < 150)
+        {
+            return "Short";
+        }
+        else if (heightCm < 180)
+        {
+            return "Average";
+        }
+        else
+        {
+            return "Tall";
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Solution obj = new Solution();
+
+        int heightCm = int.Parse(Console.ReadLine());
+
+        string category = obj.DisplayHeight(heightCm);
+
+        Console.WriteLine(category);
+    }
+}
