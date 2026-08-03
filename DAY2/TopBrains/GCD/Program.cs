efebdb8a961481170d32f444f82f183a5638a8e2
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-03 23:47:37 +0530

    GCD

---
+++ DAY2/TopBrains/GCD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Solution
+{
+    public int GCD(int a, int b)
+    {
+        if (b == 0)
+            return a;
+
+        return GCD(b, a % b);
+    }
+
+    public static void Main()
+    {
+        Solution obj = new Solution();
+
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(obj.GCD(a, b));
+    }
+}
