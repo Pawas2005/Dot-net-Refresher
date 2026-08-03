a5ebaecc84401dc9a0e9cdf733d4412ed3808c0a
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-03 17:12:00 +0530

    ArithmeticExpressions

---
+++ DAY2/TopBrains/ArithmeticExpressions/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Solution
+{
+    public string EvaluateExpression(string expression)
+    {
+        if (string.IsNullOrWhiteSpace(expression))
+            return "Error:InvalidExpression";
+
+        string[] parts = expression.Split(' ');
+
+        if (parts.Length != 3)
+            return "Error:InvalidExpression";
+
+        int a, b;
+
+        if (!int.TryParse(parts[0], out a) || !int.TryParse(parts[2], out b))
+            return "Error:InvalidNumber";
+
+        string op = parts[1];
+
+        switch (op)
+        {
+            case "+":
+                return (a + b).ToString();
+
+            case "-":
+                return (a - b).ToString();
+
+            case "*":
+                return (a * b).ToString();
+
+            case "/":
+                if (b == 0)
+                    return "Error:DivideByZero";
+
+                return (a / b).ToString();
+
+            default:
+                return "Error:UnknownOperator";
+        }
+    }
+
+    public static void Main()
+    {
+        Solution obj = new Solution();
+
+        string expression = Console.ReadLine();
+
+        Console.WriteLine(obj.EvaluateExpression(expression));
+    }
+}
