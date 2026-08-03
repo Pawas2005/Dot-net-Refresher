61b9c1371c0e910f4ccad1b7ce1b1ed1c0c5b2b5
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-03 23:50:39 +0530

    Null Value

---
+++ DAY2/TopBrains/NullValue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Solution
+{
+    public double? AverageNonNull(double?[] values)
+    {
+        double sum = 0;
+        int count = 0;
+
+        foreach (double? value in values)
+        {
+            if (value.HasValue)
+            {
+                sum += value.Value;
+                count++;
+            }
+        }
+
+        if (count == 0)
+            return null;
+
+        return Math.Round(sum / count, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static void Main()
+    {
+        Solution obj = new Solution();
+
+        double?[] values = { 10.5, null, 20.5, 30.0, null };
+
+        double? result = obj.AverageNonNull(values);
+
+        if (result.HasValue)
+            Console.WriteLine(result.Value);
+        else
+            Console.WriteLine("null");
+    }
+}
