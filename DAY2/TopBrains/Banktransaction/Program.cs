a97db8e4d3063ff6f5d0c8c3f7c1194683b8e2f1
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-03 20:37:26 +0530

    Bank transaction program

---
+++ DAY2/TopBrains/Banktransaction/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Solution
+{
+    public int FinalBalance(int initialBalance, int[] transactions)
+    {
+        int balance = initialBalance;
+
+        foreach (int transaction in transactions)
+        {
+            if (transaction >= 0)
+            {
+                // Deposit
+                balance += transaction;
+            }
+            else
+            {
+                // Withdraw only if enough balance
+                if (balance >= -transaction)
+                {
+                    balance += transaction; // transaction is negative
+                }
+            }
+        }
+
+        return balance;
+    }
+
+    public static void Main()
+    {
+        Solution obj = new Solution();
+
+        int initialBalance = 1000;
+        int[] transactions = { 500, -200, -1500, 300, -400 };
+
+        int finalBalance = obj.FinalBalance(initialBalance, transactions);
+
+        Console.WriteLine(finalBalance);
+    }
+}
