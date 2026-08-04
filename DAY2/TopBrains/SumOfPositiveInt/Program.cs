e292abecd2241d75dd96114139f29782bcf3c060
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-04 10:26:46 +0530

    Sum Of Positive Integer

---
+++ DAY2/TopBrains/SumOfPositiveInt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Solution
+{
+    public int SumPositiveIntegers(int[] nums)
+    {
+        int sum = 0;
+
+        foreach (int num in nums)
+        {
+            // Stop if 0 is encountered
+            if (num == 0)
+                break;
+
+            // Ignore negative numbers
+            if (num < 0)
+                continue;
+
+            // Add positive numbers
+            sum += num;
+        }
+
+        return sum;
+    }
+
+    public static void Main()
+    {
+        Solution obj = new Solution();
+
+        int[] nums = { 5, -2, 8, 3, -1, 0, 10, 20 };
+
+        Console.WriteLine(obj.SumPositiveIntegers(nums));
+    }
+}
