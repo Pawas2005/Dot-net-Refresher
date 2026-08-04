4f6becb0ee4b49ae4e8707881327c4fe4ad42589
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-04 16:07:56 +0530

    Bike Rental Assignment

---
+++ DAY2/TopBrains/BikeRentalAssignment/Bike.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace BikeRentalAssignment;
+
+public class Bike
+{
+    public string Model { get; set; }
+    public int PricePerDay { get; set; }
+    public string Brand { get; set; }
+}
