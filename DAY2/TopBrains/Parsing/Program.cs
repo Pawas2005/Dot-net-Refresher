80cbacc26df406998d903b6aae1ab64e2f858560
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-04 14:17:57 +0530

    Parsing

---
+++ DAY2/TopBrains/Parsing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Solution
+{
+    public int SumParsedIntegers(string[] tokens)
+    {
+        int sum = 0;
+
+        foreach (string token in tokens)
+        {
+            int value;
+
+            if (int.TryParse(token, out value))
+            {
+                sum += value;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void Main()
+    {
+        Solution obj = new Solution();
+
+        string[] tokens = { "10", "20", "abc", "2147483648", "-5", "100" };
+
+        Console.WriteLine(obj.SumParsedIntegers(tokens));
+    }
+}
