881bae0bf1b0a773c265c8ac4ef8dd2a6d82f8be
Pawas Ranjan <pawasranjan5@gmail.com>
2026-08-04 11:53:15 +0530

    File I/O – Read & Filter Logs

---
+++ DAY2/TopBrains/FileInpOp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+public class Solution
+{
+    public static void Main()
+    {
+        string inputFile = "log.txt";
+        string outputFile = "error.txt";
+
+        if (File.Exists(inputFile))
+        {
+            string[] logs = File.ReadAllLines(inputFile);
+
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                foreach (string log in logs)
+                {
+                    if (log.Contains("ERROR"))
+                    {
+                        writer.WriteLine(log);
+                    }
+                }
+            }
+
+            Console.WriteLine("ERROR logs extracted successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Input file not found.");
+        }
+    }
+}
